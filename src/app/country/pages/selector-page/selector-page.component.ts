4511d576726cfb70403661e43f84fdcfe3d50134
Heider Rey Hernandez <heiderhrnndz@gmail.com>
2023-06-30 18:09:21 -0500

    Proyecto iniciado

---
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+  styles: [
+  ]
+})
+export class SelectorPageComponent {
+
+  public myForm = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    borders: ['', Validators.required],
+  })
+
+  constructor(private fb: FormBuilder) {}
+}
